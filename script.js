c96979d2fb800b8895df55c3dfcb4867143fb923
imtomasz <thomas.brs@outlook.fr>
2026-04-01 16:15:29 +0200

    Restructuration + Factorisation du code JS

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-let identif = 1;
-let numnum = document.getElementById(identif);
+
+// date du jour en entier (ex : 1, 4, 17, 22 ...)
 let date = new Date()
 let jour = date.getDate()
 
-document.querySelectorAll('.position-relative').forEach(day => {
+/**
+ * indicateur de progression de l'ouverture des cases, sera incrémenté de 1 à chaque clic sur une case valide
+ */
+let progression = 1
+
+/**
+* Cette fonction crée un événement de clic sur chaque case. 
+* Elle compare ensuite, lors du clic, l'index de progression et la date du jour, elle laisse apparaître l'image si la case peut être ouverte
+*/
+const canItBeOpened = document.querySelectorAll('.position-relative').forEach(day => {
+  
   day.addEventListener('click', (event) => {
 
-        // vérification de l'élément ciblé
-        console.log(parseInt(event.currentTarget.id))
-
-        // 
-        if (parseInt(event.currentTarget.id)===identif && parseInt(event.currentTarget.id)<=jour ) {
-        day.classList.toggle('revealed');
-        identif++
-        numnum = document.getElementById(identif);
-        if (identif===25) {
-            showPopup()
-        }
-
-        // pour verification de l'incrémentation
-        console.log(identif) 
-        console.log(numnum)
-        }
-        if (parseInt(event.currentTarget.id)>identif+1){ 
-          alert("Ce n'est pas le bon jour !")
-        }
-        if (parseInt(event.currentTarget.id)===identif+1) {
-          alert("Cette case s'ouvrira demain !")
+        // comparaison de la progression et de la date
+        
+        switch (true) {
+          case (parseInt(event.currentTarget.id) === progression + 1) :
+            alert("Cette case s'ouvrira demain !")
+            break
+          case (parseInt(event.currentTarget.id) > progression + 1) : 
+            alert("Ce n'est pas le bon jour !")
+            break
+          case (parseInt(event.currentTarget.id) <= jour && parseInt(event.currentTarget.id) === progression) :
+            day.classList.toggle('revealed');
+            progression++
+            if (progression===25) {
+                showPopup()
+            }
+            break
         }
   });
 });
